c0134966b6ee94517264d6ec8aa869d9f9a49bd7
oswaldobapvicjr <oswaldo.bapvic.jr@outlook.com>
2018-12-07 08:54:52 -0200

    Update JEPContextFactory to allow object creation with initial variables

--- src/main/java/net/obvj/jep/JEPContextFactory.java
+++ src/main/java/net/obvj/jep/JEPContextFactory.java
@@ -1,8 +1,12 @@
 package net.obvj.jep;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.nfunk.jep.JEP;
 import org.nfunk.jep.OperatorSet;
 import org.nfunk.jep.function.Comparative;
+import org.nfunk.jep.function.PostfixMathCommand;
 import org.nfunk.jep.type.NumberFactory;
 
 import net.obvj.jep.functions.*;
@@ -21,11 +25,21 @@ public class JEPContextFactory
 
     /**
      * @return a new {@code JEP} object with custom functions and operators registered and
-     *         default, preferred behavior
+     *         default behavior
      */
     public static JEP newContext()
     {
-        return newContext(false, true, false, null);
+        return newContext(null, false, true, false, null);
+    }
+
+    /**
+     * @param contextMap a map of variables to be used by the evaluation context
+     * @return a new {@code JEP} object with initial variables in the context and custom
+     *         functions and operators registered and default behavior
+     */
+    public static JEP newContext(Map<String, Object> contextMap)
+    {
+        return newContext(contextMap, false, true, false, null);
     }
 
     /**
@@ -39,35 +53,63 @@ public class JEPContextFactory
     public static JEP newContext(boolean traverse, boolean allowUndeclared, boolean implicitMultiplication,
             NumberFactory numberFactory)
     {
-        JEP context = new JEP(traverse, allowUndeclared, implicitMultiplication, numberFactory);
+        return newContext(null, traverse, allowUndeclared, implicitMultiplication, numberFactory);
+    }
 
-        context.addStandardFunctions();
+    /**
+     * @param contextMap             a map of variables to be used by the evaluation context
+     * @param traverse               an optional to print expression trees (useful for debug)
+     * @param allowUndeclared        the "allow undeclared variables" option
+     * @param implicitMultiplication the "implicit multiplication" option
+     * @param numberFactory          the number factory to be used
+     *
+     * @return a new {@code JEP} object with custom functions and operators registered
+     */
+    public static JEP newContext(Map<String, Object> contextMap, boolean traverse, boolean allowUndeclared,
+            boolean implicitMultiplication, NumberFactory numberFactory)
+    {
+        JEP jep = new JEP(traverse, allowUndeclared, implicitMultiplication, numberFactory);
 
+        jep.addStandardFunctions();
+        addCustomFunctions(jep);
+
+        if (contextMap != null) addVariables(jep, contextMap);
+
+        return jep;
+    }
+
+    /**
+     * Registers custom functions and operators into JEP
+     *
+     * @param jep the JEP object where functions will be registered
+     */
+    public static void addCustomFunctions(JEP jep)
+    {
         // String functions
-        context.addFunction("concat", new Concat());
-        context.addFunction("lower", new Lower());
-        context.addFunction("replace", new Replace());
-        context.addFunction("trim", new Trim());
-        context.addFunction("upper", new Upper());
+        jep.addFunction("concat", new Concat());
+        jep.addFunction("lower", new Lower());
+        jep.addFunction("replace", new Replace());
+        jep.addFunction("trim", new Trim());
+        jep.addFunction("upper", new Upper());
 
         // Date functions
-        context.addFunction("now", new Now());
+        jep.addFunction("now", new Now());
 
         // Data manipulation functions
-        context.addFunction("jsonpath", new JsonPath());
+        jep.addFunction("jsonpath", new JsonPath());
 
         // Statistical functions
-        context.addFunction("count", new Count());
+        jep.addFunction("count", new Count());
 
         // Random functions
-        context.addFunction("uuid", new UUID());
+        jep.addFunction("uuid", new UUID());
 
         // Utility functions
-        context.addFunction("getSystemProperty", new SystemPropertyReader());
-        context.addFunction("getEnv", new EnvironmentVariableReader());
+        jep.addFunction("getSystemProperty", new SystemPropertyReader());
+        jep.addFunction("getEnv", new EnvironmentVariableReader());
 
         // Operators
-        OperatorSet operators = context.getOperatorSet();
+        OperatorSet operators = jep.getOperatorSet();
         operators.getLT().setPFMC(new DateAwareComparative(Comparative.LT));
         operators.getGT().setPFMC(new DateAwareComparative(Comparative.GT));
         operators.getLE().setPFMC(new DateAwareComparative(Comparative.LE));
@@ -75,8 +117,25 @@ public class JEPContextFactory
         operators.getNE().setPFMC(new DateAwareComparative(Comparative.NE));
         operators.getEQ().setPFMC(new DateAwareComparative(Comparative.EQ));
 
-        operators.getElement().setPFMC(new Element());
+        // Element function and operator
+        PostfixMathCommand elementCommand = new Element();
+        jep.addFunction("getelement", elementCommand);
+        operators.getElement().setPFMC(elementCommand);
+    }
 
-        return context;
+    /**
+     * Adds all entries from the {@code contextMap} as variables in JEP context
+     *
+     * @param jep        the JEP object to be filled with variables
+     * @param contextMap a map of variables to be used by the evaluation context
+     */
+    public static void addVariables(JEP jep, Map<String, Object> contextMap)
+    {
+        if (jep == null) throw new IllegalArgumentException("A null JEP object was received");
+
+        if (contextMap == null) throw new IllegalArgumentException("A null context map was received");
+
+        for (Entry<String, Object> entry : contextMap.entrySet())
+            jep.addVariable(entry.getKey(), entry.getValue());
     }
 }
