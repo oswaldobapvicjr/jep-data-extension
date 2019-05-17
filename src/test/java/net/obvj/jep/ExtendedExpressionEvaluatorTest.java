b14a4e0bfbb4ec6e153c17bc3cb27ce38f808c9e
oswaldobapvicjr <oswaldo.bapvic.jr@outlook.com>
2019-05-17 18:59:43 -0300

    New tests involving the httpGet() function together with jsonpath() and others

--- src/test/java/net/obvj/jep/ExtendedExpressionEvaluatorTest.java
+++ src/test/java/net/obvj/jep/ExtendedExpressionEvaluatorTest.java
@@ -2,19 +2,39 @@ package net.obvj.jep;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.ws.rs.core.MediaType;
+
+import org.codehaus.jettison.json.JSONException;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.nfunk.jep.ParseException;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.ClientResponse.Status;
+import com.sun.jersey.api.client.WebResource;
+
+import net.obvj.jep.util.JsonUtils;
 
 /**
  * Unit tests for the {@link ExtendedExpressionEvaluator} class.
  *
  * @author oswaldo.bapvic.jr
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Client.class)
 public class ExtendedExpressionEvaluatorTest
 {
     // Test variable names
@@ -61,6 +81,56 @@ public class ExtendedExpressionEvaluatorTest
             "   }\r\n" +
             "}";
 
+    // ----------------------
+    // Web Services test data
+    // ----------------------
+
+    private static final String URL_EMPLOYEES = "http://localhost/services/employees";
+    private static final String URL_EMPLOYEE1 = "http://localhost/services/employee/1";
+
+    private static final String JSON_EMPLOYEES = "[\r\n" +
+            "   {\"id\":1,\"name\":\"Matthew\"},\r\n" +
+            "   {\"id\":2,\"name\":\"Mark\"},\r\n" +
+            "   {\"id\":3,\"name\":\"Luke\"},\r\n" +
+            "   {\"id\":4,\"name\":\"John\"}\r\n" +
+            "]";
+
+    private static final String JSON_EMPLOYEE1 = "{\"id\":1,\"name\":\"Matthew\"}";
+    private static final String JSON_EMPLOYEE1_NAME = "Matthew";
+
+    // Expressions with Web Services
+    private static final String EXPRESSION_COUNT_HTTP_GET = "count(httpGet(\"http://localhost/services/employees\"))";
+    private static final String EXPRESSION_GET_HTTP_GET = "get(httpGet(\"http://localhost/services/employees\"), 1)";
+    private static final String EXPRESSION_JSONPATH_HTTP_GET = "jsonpath(httpGet(\"http://localhost/services/employee/1\"), \"$.name\")";
+    private static final String EXPRESSION_JSONPATH_HTTP_GET_WITH_PLACEHOLDER = "jsonpath(httpGet(\"http://localhost/services/employee/${myId}\"), \"$.name\")";
+
+    @Mock
+    private ClientResponse clientResponse;
+
+    @Mock
+    private WebResource webResource;
+
+    @Mock
+    private Client client;
+
+    /**
+     * Utility method to mock the response from an HTTP request
+     *
+     * @param url the URL to be passed to the mock
+     * @param mediaType the media type to be set
+     * @param expectedResponse the content to be set
+     */
+    private void mockGetWebServiceResponse(String url, MediaType mediaType, String expectedResponse)
+    {
+        PowerMockito.mockStatic(Client.class);
+        PowerMockito.when(Client.create()).thenReturn(client);
+
+        when(client.resource(url)).thenReturn(webResource);
+        when(webResource.get(ClientResponse.class)).thenReturn(clientResponse);
+        when(clientResponse.getClientResponseStatus()).thenReturn(Status.OK);
+        when(clientResponse.getType()).thenReturn(mediaType);
+        when(clientResponse.getEntity(String.class)).thenReturn(expectedResponse);
+    }
 
     /**
      * Tests that the component is not created with a null expression
@@ -198,6 +268,67 @@ public class ExtendedExpressionEvaluatorTest
     {
         ExtendedExpressionEvaluator evaluator = new ExtendedExpressionEvaluator(EXPRESSION_GOOD_PLUS_PERIOD);
         evaluator.evaluate(Collections.emptyMap());
+    }
+
+    /**
+     * Tests component execution with the count() and httpGet() functions nested
+     *
+     * @throws ParseException
+     */
+    @Test
+    public void testComponentExecutionWithCountAndHttpGet() throws ParseException
+    {
+        mockGetWebServiceResponse(URL_EMPLOYEES, MediaType.APPLICATION_JSON_TYPE, JSON_EMPLOYEES);
+        ExtendedExpressionEvaluator evaluator = new ExtendedExpressionEvaluator(EXPRESSION_COUNT_HTTP_GET);
+        assertEquals(4, evaluator.evaluate(Collections.emptyMap()));
+    }
+
+    /**
+     * Tests component execution with the get() and httpGet() functions nested
+     *
+     * @throws ParseException
+     * @throws JSONException
+     */
+    @Test
+    public void testComponentExecutionWithGetAndHttpGet() throws ParseException, JSONException
+    {
+        mockGetWebServiceResponse(URL_EMPLOYEES, MediaType.APPLICATION_JSON_TYPE, JSON_EMPLOYEES);
+        ExtendedExpressionEvaluator evaluator = new ExtendedExpressionEvaluator(EXPRESSION_GET_HTTP_GET);
+        assertEquals(JsonUtils.convertToJSONObject(JSON_EMPLOYEE1), evaluator.evaluate(Collections.emptyMap()));
+    }
+
+    /**
+     * Tests component execution with the jsonpath() and httpGet() functions nested
+     *
+     * @throws ParseException
+     */
+    @Test
+    public void testComponentExecutionWithJsonpathAndHttpGet() throws ParseException
+    {
+        mockGetWebServiceResponse(URL_EMPLOYEE1, MediaType.APPLICATION_JSON_TYPE, JSON_EMPLOYEE1);
+        ExtendedExpressionEvaluator evaluator = new ExtendedExpressionEvaluator(EXPRESSION_JSONPATH_HTTP_GET);
+        assertEquals(JSON_EMPLOYEE1_NAME, evaluator.evaluate(Collections.emptyMap()));
+    }
+
+    /**
+     * Tests component execution with the jsonpath() and httpGet() functions nested, and a
+     * placeholder in the URL
+     *
+     * @throws ParseException
+     */
+    @Test
+    public void testComponentExecutionWithJsonpathAndHttpGetAndPlaceholderInUrl() throws ParseException
+    {
+        mockGetWebServiceResponse(URL_EMPLOYEE1, MediaType.APPLICATION_JSON_TYPE, JSON_EMPLOYEE1);
+        ExtendedExpressionEvaluator evaluator = new ExtendedExpressionEvaluator(
+                EXPRESSION_JSONPATH_HTTP_GET_WITH_PLACEHOLDER);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("myId", 1);
+        assertEquals(JSON_EMPLOYEE1_NAME, evaluator.evaluate(map));
+
+        // Verify that the URL without place-holders was passed to the mocked HTTP client
+        verify(client, times(1)).resource(URL_EMPLOYEE1);
     }
 
 }
