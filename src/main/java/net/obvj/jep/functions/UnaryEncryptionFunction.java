a167af321bb61e796631a6faffdcfe13f44b2a0d
Oswaldo Baptista Vicente Junior <45291656+oswaldobapvicjr@users.noreply.github.com>
2023-01-09 23:49:14 -0300

    Fix CVE-916

--- src/main/java/net/obvj/jep/functions/UnaryEncryptionFunction.java
+++ src/main/java/net/obvj/jep/functions/UnaryEncryptionFunction.java
@@ -20,16 +20,6 @@ public class UnaryEncryptionFunction extends PostfixMathCommand implements Multi
      */
     public enum EncryptionAlgorithm
     {
-        @Function("md5")
-        MD5
-        {
-            @Override
-            String execute(String content)
-            {
-                return EncryptionUtils.md5(content);
-            }
-        },
-
         @Function("sha1")
         SHA1
         {
--- src/test/java/net/obvj/jep/functions/UnaryEncryptionFunctionTest.java
+++ src/test/java/net/obvj/jep/functions/UnaryEncryptionFunctionTest.java
@@ -17,22 +17,10 @@ import net.obvj.jep.util.CollectionsUtils;
  */
 public class UnaryEncryptionFunctionTest
 {
-    private static UnaryEncryptionFunction md5 = new UnaryEncryptionFunction(EncryptionAlgorithm.MD5);
     private static UnaryEncryptionFunction sha1 = new UnaryEncryptionFunction(EncryptionAlgorithm.SHA1);
     private static UnaryEncryptionFunction sha256 = new UnaryEncryptionFunction(EncryptionAlgorithm.SHA256);
     private static UnaryEncryptionFunction toBase64 = new UnaryEncryptionFunction(EncryptionAlgorithm.TO_BASE64);
     private static UnaryEncryptionFunction fromBase64 = new UnaryEncryptionFunction(EncryptionAlgorithm.FROM_BASE64);
-
-    /**
-     * Tests the MD5 function with a valid string
-     */
-    @Test
-    public void testMd5WithValidString() throws ParseException
-    {
-        Stack<Object> parameters = CollectionsUtils.newParametersStack("asd");
-        md5.run(parameters);
-        assertEquals("7815696ecbf1c96e6894b779456d330e", parameters.pop());
-    }
 
     /**
      * Tests the SHA1 function with a valid string
